0a4c4e4bb6f0a0096084e99fc746412fc96d19aa
Robichiri <robertochiriac8@gmail.com>
2023-12-06 08:59:52 +0100

    esercizio-4

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,9 @@
 import { Hello } from "./Hello";
-import { Message } from "./Message";
 
 export function App() {
   return (
     <div>
       <Hello />
-      <Hello />
-      <Message />
     </div>
   );
 }
-
-// il programma funziona regolarmente, è possibile utilizzre più volte lo stesso componente Hello
-// ed è possibile renderizzare Message direttamente in App
