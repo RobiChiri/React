cc8b5725dacb4826dee1ffdd728791423ffbd663
Robichiri <robertochiriac8@gmail.com>
2023-12-06 09:08:27 +0100

    esercizio-6

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 import { Hello } from "./Hello";
+import { Welcome } from "./Welcome";
 
 export function App() {
   return (
     <div>
       <Hello />
+      <Welcome name={<strong>Robi</strong>} />
     </div>
   );
 }
---
+++ vite-project/src/Welcome.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line react/prop-types
+export function Welcome({ name, age }) {
+  return (
+    <div>
+      <p>Welcome, {name}!</p>
+      <p>Your age is {age}.</p>
+    </div>
+  );
+}
