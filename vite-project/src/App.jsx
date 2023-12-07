26e930fe6800e897f3306fc4c481ce965ad768c3
Robichiri <robertochiriac8@gmail.com>
2023-12-07 16:23:47 +0100

    esercizio-15

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import { Welcome } from "./Welcome";
 import { AlertClock } from "./AlertClock";
 import { Counter } from "./Counter";
 import { Clock } from "./Clock";
+import { MouseClicker } from "./MouseClicker";
 
 export function App() {
   function handleButtonClick() {
@@ -16,6 +17,7 @@ export function App() {
       <AlertClock eventHandler={handleButtonClick} />
       <Counter initialValue={0} increment={3} decrement={2} />
       <Clock />
+      <MouseClicker />
     </div>
   );
 }
