d40e13ab301be1a5cf3d9baedb1e01185ea39e9d
Robichiri <robertochiriac8@gmail.com>
2023-12-07 09:10:49 +0100

    esercizio-7

--- vite-project/src/Welcome.jsx
+++ vite-project/src/Welcome.jsx
@@ -1,11 +1,13 @@
 import { Age } from "./Age";
 
 // eslint-disable-next-line react/prop-types
-export function Welcome({ name }) {
+export function Welcome({ name, age }) {
   return (
     <div>
       <p>Welcome, {name}!</p>
-      <p>{<Age age={20} />}</p>
+      <p>
+        <Age age={age} />
+      </p>
     </div>
   );
 }
