0cffcf0f4ad42935910341c256d82859af70f76f
Robichiri <robertochiriac8@gmail.com>
2023-12-07 10:18:52 +0100

    esercizio-10

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,17 @@
 import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
+import { AlertClock } from "./AlertClock";
 
 export function App() {
+  function handleButtonClick() {
+    const now = new Date();
+    alert(now.toLocaleTimeString());
+  }
   return (
     <div>
       <Hello />
       <Welcome name="John" age={17} />
+      <AlertClock onClick={handleButtonClick} />
     </div>
   );
 }
