1c5647edd7ca1aa58e32909e73a0757208003f7a
Robichiri <robertochiriac8@gmail.com>
2023-12-14 08:16:01 +0100

    esercizio-23

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,35 @@
-import { Hello } from "./Hello";
+// import { Hello } from "./Hello";
 // import { Welcome } from "./Welcome";
-import { AlertClock } from "./AlertClock";
-import { Counter } from "./Counter";
-import { Clock } from "./Clock";
-import { MouseClicker } from "./MouseClicker";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { UncontrolledForm } from "./UncontrolledForm";
+// import { AlertClock } from "./AlertClock";
+// import { Counter } from "./Counter";
+// import { Clock } from "./Clock";
+// import { MouseClicker } from "./MouseClicker";
+// import { InteractiveWelcome } from "./InteractiveWelcome";
+// import { Login } from "./Login";
+// import { UncontrolledForm } from "./UncontrolledForm";
+import { FocusableInput } from "./FocusableInput";
 
 export function App() {
-  function handleButtonClick() {
-    const now = new Date();
-    alert(now.toLocaleTimeString());
-  }
+  // function handleButtonClick() {
+  //   const now = new Date();
+  //   alert(now.toLocaleTimeString());
+  // }
 
-  function onLogin(data) {
-    console.log(data);
-  }
+  // function onLogin(data) {
+  //   console.log(data);
+  // }
   return (
     <div>
-      <Hello />
-      {/* <Welcome name="John" age={17} /> */}
+      {/* <Hello />
+      <Welcome name="John" age={17} />
       <AlertClock eventHandler={handleButtonClick} />
       <Counter initialValue={0} increment={3} decrement={2} />
       <Clock />
       <MouseClicker />
       <InteractiveWelcome />
       <Login onLogin={onLogin} />
-      <UncontrolledForm />
+      <UncontrolledForm /> */}
+      <FocusableInput />
     </div>
   );
 }
