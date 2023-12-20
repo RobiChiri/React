e7172745916e00a058c724e90335fbfef1878c34
Robichiri <robertochiriac8@gmail.com>
2023-12-20 09:50:48 +0100

    esercizio-33

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,9 +2,9 @@
 // import { Welcome } from "./Welcome";
 // import { AlertClock } from "./AlertClock";
 // import { Counter } from "./Counter";
-import { useState } from "react";
-import { Clock } from "./Clock";
-import { LanguageContext } from "./LanguageContext";
+// import { useState } from "react";
+// import { Clock } from "./Clock";
+// import { LanguageContext } from "./LanguageContext";
 // import { MouseClicker } from "./MouseClicker";
 // import { InteractiveWelcome } from "./InteractiveWelcome";
 // import { Login } from "./Login";
@@ -13,6 +13,8 @@ import { LanguageContext } from "./LanguageContext";
 // import { StrictModeComponent } from "./StrictModeComponent";
 // import { StrictMode } from "react";
 // import {Container} from "./Container"
+
+import { GithubUser } from "./GithubUser";
 
 // import { TodoList } from "./TodoList";
 
@@ -26,21 +28,22 @@ export function App() {
   //   console.log(data);
   // }
 
-  const[language, setLanguage]=useState("en")
+  // const[language, setLanguage]=useState("en")
 
-  function handleSetLanguage(language){
-    setLanguage(language)
-    console.log(language)
-  }
+  // function handleSetLanguage(language){
+  //   setLanguage(language)
+  //   console.log(language)
+  // }
   return (
     <div>
-      <select defaultValue="language">
+      <GithubUser username="RobiChiri" />
+      {/* <select defaultValue="language">
        <option onChange={()=>handleSetLanguage("en")}>EN</option> 
        <option onChange={()=>handleSetLanguage("it")}>IT</option> 
       </select>
       <LanguageContext.Provider value={language}>
         <Clock />
-      </LanguageContext.Provider>
+      </LanguageContext.Provider> */}
       {/* <Hello /> */}
       {/* <Welcome name="John" age={17} /> */}
       {/* <AlertClock eventHandler={handleButtonClick} /> */}
